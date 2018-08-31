251dba5b1df831dcd59b75c798acd524005c3f87
Mohammed Ameen <jegroamin@gmail.com>
2018-08-31 14:20:35 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -25,19 +25,23 @@ export class OlderCoaster extends React.Component {
 export class InFrontOfYou extends React.Component {
    render(){
     return (
-    <div class="oldercoaster">
-  <p>Two grannies having the time of their life!</p>
-  <p>Passengers:</p>
-  <ul>
-    <li>Agnes</li>
-    <li>Muriel</li>
-  </ul>
-</div>)
+    <div>
+  <p>You shouldn't look too far.</p>
+  <p>Sometimes, the solution is right in front of you.</p>
+</div>
+    )
 }
 }
 
 export class ButcherShop extends React.Component {
-  // your code here
+  render(){
+    return (
+    <div>
+  <p>You shouldn't look too far.</p>
+  <p>Sometimes, the solution is right in front of you.</p>
+</div>
+    )
+}
 }
 
 
